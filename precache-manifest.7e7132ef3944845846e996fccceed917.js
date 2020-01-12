f1b9b74a1f6b20835583400a74f0fe2970b2c61c
Pantilei <ianulov.pantilei@gmail.com>
2020-01-12 11:43:30 +0200

    Updates

---
+++ precache-manifest.7e7132ef3944845846e996fccceed917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3473dc32163f526653f84890179974",
+    "url": "/robo_app/index.html"
+  },
+  {
+    "revision": "fced60f829d2bece9222",
+    "url": "/robo_app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d75122c70d4c8f411acf",
+    "url": "/robo_app/static/css/main.17fe0e93.chunk.css"
+  },
+  {
+    "revision": "fced60f829d2bece9222",
+    "url": "/robo_app/static/js/2.9394ebe9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robo_app/static/js/2.9394ebe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d75122c70d4c8f411acf",
+    "url": "/robo_app/static/js/main.f6574fc5.chunk.js"
+  },
+  {
+    "revision": "63eae63bc80d378c6b0d",
+    "url": "/robo_app/static/js/runtime-main.69b6a42b.js"
+  },
+  {
+    "revision": "5e05d68cbf8db59ba03f91507e84dac3",
+    "url": "/robo_app/static/media/SK_BRIAN.5e05d68c.ttf"
+  }
+]);
